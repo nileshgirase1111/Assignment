92b889f60bc29b6bfcd5959fddf916fb07753ae0
Nilesh <93610140+nileshgirase1111@users.noreply.github.com>
2021-11-16 22:29:26 +0530

    Add files via upload

---
+++ week 1/PrintEvenNumber.java
@@ -0,0 +1,18 @@
+public class PrintEvenNumber {
+public static void main(String[] args) {
+		System.out.println("using for loop print number");
+		for(int i = 0;i <= 10;i=i+2) {
+			System.out.println(i);
+		}
+		
+		System.out.println("using while loop print number");
+		
+		int k = 0;
+		while(k<=10) {
+			System.out.println(k);
+			k= k+2;
+		}
+
+	}
+
+}
